101475c86c4c7c318298682d42f25311fa3a829a
Ernie <jasonernie03@gmail.com>
2020-11-07 00:36:09 -0500

    move dotes based on input image

---
+++ midTerm/Assets/Scripts/animatedObjects.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class animatedObjects : MonoBehaviour
+{
+    public GameObject redGO;
+    public GameObject blueGO;
+    public extractImageInfo ex;
+    Dictionary<string, List<Vector3>> coordinatesDic = new Dictionary<string, List<Vector3>>();
+    static List<GameObject> redGOLists = new List<GameObject>();
+    static List<GameObject> blueGOLists = new List<GameObject>();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coordinatesDic = ex.GetRednBlueCoordinates();
+        loadInitialStates();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animatingObjects();
+    }
+
+    void loadInitialStates() {
+        for (int i = 0; i < coordinatesDic["red"].Count; i++)
+        {
+            Vector3 redCoord = coordinatesDic["red"][i];
+            redGOLists.Add(Instantiate(redGO, redCoord, Quaternion.identity) as GameObject);
+        }
+
+        for (int i = 0; i < coordinatesDic["blue"].Count; i++ )
+        {
+            Vector3 blueCoord = coordinatesDic["blue"][i];
+            blueGOLists.Add(Instantiate(blueGO, blueCoord, Quaternion.identity) as GameObject);
+        }
+    }
+
+    void animatingObjects() {
+        for (int i = 0; i < redGOLists.Count; i++)
+        {
+            GameObject redG = redGOLists[i];
+            NavMeshAgent redAgent = redG.GetComponent<NavMeshAgent>();
+            redAgent.SetDestination(new Vector3(coordinatesDic["red"][i].x - 100f, coordinatesDic["red"][i].y, coordinatesDic["red"][i].z));
+        }
+        
+        for (int j = 0; j < blueGOLists.Count; j++)
+        {
+            Debug.Log("???");
+            GameObject blueG = blueGOLists[j];
+            NavMeshAgent blueAgent = blueG.GetComponent<NavMeshAgent>();
+
+            
+            blueAgent.SetDestination(new Vector3(coordinatesDic["blue"][j].x - 100f, coordinatesDic["blue"][j].y, coordinatesDic["blue"][j].z));
+        }
+    }
+}
---
+++ midTerm/Assets/Scripts/extractImageInfo.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class extractImageInfo : MonoBehaviour
+{
+    public Texture2D CSImage;
+
+    private void Awake()
+    {
+        GetRednBlueCoordinates();
+    }
+
+    public Dictionary<string, List<Vector3>> GetRednBlueCoordinates()
+    {
+        Dictionary<string, List<Vector3>> coordinatesDic = new Dictionary<string, List<Vector3>>();
+        List<Vector3> blueDots = new List<Vector3>();
+        List<Vector3> redDots = new List<Vector3>();
+
+        for (int i = 0; i < CSImage.width; i++) 
+        {
+            for (int j = 0; j < CSImage.height; j++)
+            {
+                Color pixel = CSImage.GetPixel(i, j);
+
+                if ((pixel.b) * 255f > 10 && (pixel.b) * 255f < 27) // blue dots feature
+                {
+                    blueDots.Add(new Vector3((float)i, 0.5f, (float)j));
+                }
+                else if (((pixel.r) * 255f > 20 && (pixel.r) * 255f < 50)) // red dots feature
+                {
+                    redDots.Add(new Vector3((float)i, 0.5f, (float)j));
+                }
+            }
+        }
+        coordinatesDic.Add("blue", blueDots);
+        coordinatesDic.Add("red", redDots);
+
+        return coordinatesDic;
+    }
+
+
+
+
+
+}
